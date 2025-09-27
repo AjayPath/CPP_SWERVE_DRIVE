0a0f22e224aed61fbef37d7b321fdfc13321df4c
AjayPath <ajaypath3@gmail.com>
2025-09-26 21:24:25 -0400

    Added AutoMode Structure

--- src/main/include/AutoMode.h
+++ src/main/include/AutoMode.h
@@ -4,7 +4,66 @@
 
 #pragma once
 
+#include <units/math.h>
+#include <units/length.h>
+#include "IO/PigeonGyro.h"
+#include "Constants.h"
+#include "SubsystemIncludes.h"
+
 class AutoMode {
  public:
-  AutoMode();
+  
+  int id;
+  int step;
+  std::string name;
+  units::degree_t initialAngle;
+
+  // Subsystems
+  PigeonGyro* Gyro;
+
+  // Timers
+  frc::Timer* autoTimer;
+  frc::Timer* totalAutoTimer;
+
+  AutoMode() {
+    autoTimer = new frc::Timer();
+    totalAutoTimer = new frc::Timer();
+
+    // Get instances of drivebase
+
+    initialAngle = 0_deg; // Depends on auto config
+    Gyro->SetAngle(initialAngle.value());
+    step = 0;
+  }
+
+  ~AutoMode() {}
+
+  virtual void Run() {};
+  virtual void ReflectIfRed() {};
+
+  std::string GetName() {
+    return name;
+  };
+
+  void ResetAuto() {
+    step = 0;
+    Gyro->SetAngle(initialAngle.value());
+    totalAutoTimer->Reset();
+    totalAutoTimer->Stop();
+  }
+
+  void ResetTimer() {
+    autoTimer->Reset();
+    autoTimer->Start();
+  }
+
+  int GetStep() {
+    return step;
+  }
+
+  // Vector offsets
+
+  double speed = 50; // 180
+  units::second_t timeout = 1.0_s;
+
 };
